de3cd4e14464355d42b5c9c5f6e4a96607b1366b
gscho <greg.c.schofield@gmail.com>
2018-11-08 22:39:47 -0500

    Add cookbook template
    Signed-off-by: gscho <greg.c.schofield@gmail.com>

---
+++ lib/kamehameha/templates/cookbook/metadata.rb
@@ -0,0 +1,9 @@
+name 'kamehameha'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures kamehameha'
+long_description 'Installs/Configures kamehameha'
+version '0.1.0'
+
+depends 'habitat'
