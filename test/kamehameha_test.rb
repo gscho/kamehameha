33a4cce684bba0b4d1427468cd11762c2e223382
gscho <greg.c.schofield@gmail.com>
2018-11-08 22:50:24 -0500

    Test cookbook generate
    Signed-off-by: gscho <greg.c.schofield@gmail.com>

--- test/kamehameha_test.rb
+++ test/kamehameha_test.rb
@@ -6,6 +6,6 @@ class KamehamehaTest < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    ::Kamehameha::CLI.new.exec('.')
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "kamehameha"
+require "kamehameha/cli"
 
 require "minitest/autorun"
